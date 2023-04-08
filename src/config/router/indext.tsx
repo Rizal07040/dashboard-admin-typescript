7fd3bdb38ed64106fb6a9558bd78975493295973
Rizal07040 <adirizal775@gmail.com>
2023-04-09 01:54:58 +0700

    [Rizal] setup project and add framework material ui

---
+++ src/config/router/indext.tsx
@@ -0,0 +1,34 @@
+import MyComponent from "../../components/coba1";
+
+export const router = [
+    {
+        icons: '',
+        label: 'Inbox',
+        path: '/inbox',
+        template: 'dashboard',
+        component: MyComponent
+
+    },
+    {
+        icons: '',
+        label: 'Email',
+        path: '/email',
+        template: 'dashboard',
+        component: MyComponent
+
+    },    {
+        icons: '',
+        label: 'Send Email',
+        path: '/send-email',
+        template: 'dashboard',
+        component: MyComponent
+
+    },    {
+        icons: '',
+        label: 'Trash',
+        path: '/trash',
+        template: 'dashboard',
+        component: MyComponent
+
+    }
+]
